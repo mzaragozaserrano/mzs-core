9f593eafd621dfd5898c9c2418d5e391154fcba8
Miguel Zaragoza Serrano <mzaragozaserrano@gmail.com>
2023-10-23 14:01:55 +0200

    adding component category

--- app/src/main/kotlin/presentationapp/ui/screens/CategoryListScreen.kt
+++ app/src/main/kotlin/presentationapp/ui/screens/CategoryListScreen.kt
@@ -1,15 +1,18 @@
 package presentationapp.ui.screens
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.mzaragozaserrano.compose.composables.buttons.WavyButton
 import com.mzaragozaserrano.compose.composables.utils.Recycler
+import presentationapp.ui.components.Category
 import presentationapp.ui.utils.CategoryType
 import presentationapp.ui.utils.createCategoryList
 
@@ -19,21 +22,18 @@ fun CategoryListScreen(modifier: Modifier = Modifier, onCategoryClicked: (Catego
         modifier = modifier
             .background(MaterialTheme.colorScheme.background)
             .padding(all = 16.dp),
+        hasLine = false,
         list = createCategoryList(),
-        spaceBetween = 16.dp,
-        hasLine = false
+        numberCells = 2,
+        spaceBetween = 16.dp
     ) { category ->
-        WavyButton(
-            modifier = Modifier.fillMaxWidth(),
-            buttonBackgroundColor = MaterialTheme.colorScheme.secondaryContainer,
-            iconBackgroundColor = MaterialTheme.colorScheme.tertiaryContainer,
-            iconId = category.imageId,
-            iconTint = MaterialTheme.colorScheme.onTertiaryContainer,
-            textId = category.textId,
-            textColor = MaterialTheme.colorScheme.onSecondaryContainer
-        ) {
-            onCategoryClicked(category)
-        }
+        Category(modifier = Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(12.dp))
+            .clickable { onCategoryClicked(category) },
+            imageId = category.imageId,
+            textId = category.textId
+        )
     }
 }
 
