cdc788099c02ec3b635abea4c78837f4a5fc10c3
Miguel Zaragoza Serrano <mzaragozaserrano@gmail.com>
2023-10-23 14:02:45 +0200

    now is possible to change size

--- presentation/src/main/kotlin/com/mzaragozaserrano/compose/composables/images/ResourceImage.kt
+++ presentation/src/main/kotlin/com/mzaragozaserrano/compose/composables/images/ResourceImage.kt
@@ -2,11 +2,14 @@ package com.mzaragozaserrano.compose.composables.images
 
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun ResourceImage(
@@ -14,9 +17,10 @@ fun ResourceImage(
     contentDescription: String? = null,
     @DrawableRes iconId: Int,
     iconTint: Color? = null,
+    size: Dp = 24.dp,
 ) {
     Image(
-        modifier = modifier,
+        modifier = modifier.size(size),
         contentDescription = contentDescription,
         colorFilter = iconTint?.let { color ->
             ColorFilter.tint(color = color)
