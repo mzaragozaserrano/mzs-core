c173f3511edbf595346600386e3332644442e9cf
Miguel Zaragoza Serrano <mzaragozaserrano@gmail.com>
2023-10-23 21:11:30 +0200

    creating minimal button

--- app/src/main/kotlin/presentationapp/ui/screens/CategoryListScreen.kt
+++ app/src/main/kotlin/presentationapp/ui/screens/CategoryListScreen.kt
@@ -1,22 +1,15 @@
 package presentationapp.ui.screens
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.mzaragozaserrano.compose.composables.buttons.MinimalButton
 import com.mzaragozaserrano.compose.composables.utils.Recycler
-import presentationapp.ui.components.Category
 import presentationapp.ui.utils.CategoryType
 import presentationapp.ui.utils.createCategoryList
 
@@ -31,22 +24,15 @@ fun CategoryListScreen(modifier: Modifier = Modifier, onCategoryClicked: (Catego
         numberCells = 3,
         spaceBetween = 16.dp
     ) { category ->
-        Category(modifier = Modifier
-            .fillMaxSize()
-            .clip(RoundedCornerShape(12.dp))
-            .clickable(
-                indication = rememberRipple(
-                    color = MaterialTheme.colorScheme.background.copy(alpha = 0.2f)
-                ),
-                interactionSource = remember { MutableInteractionSource() },
-                onClick = {
-                    onCategoryClicked(category)
-                }
-            )
-            .aspectRatio(1f),
-            imageId = category.imageId,
+        MinimalButton(
+            modifier = Modifier.fillMaxSize(),
+            iconId = category.imageId,
+            iconTint = MaterialTheme.colorScheme.onBackground,
+            textColor = MaterialTheme.colorScheme.onBackground,
             textId = category.textId
-        )
+        ) {
+            onCategoryClicked(category)
+        }
     }
 }
 
