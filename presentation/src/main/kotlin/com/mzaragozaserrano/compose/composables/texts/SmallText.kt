b56a3c9ae9c01b74352745783d590ed8a5c8acd2
Miguel Zaragoza Serrano <mzaragozaserrano@gmail.com>
2023-10-23 20:59:36 +0200

    adding small text

---
+++ presentation/src/main/kotlin/com/mzaragozaserrano/compose/composables/texts/SmallText.kt
@@ -0,0 +1,80 @@
+package com.mzaragozaserrano.compose.composables.texts
+
+import androidx.annotation.StringRes
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import com.mzaragozaserrano.compose.utils.FontSize
+import com.mzaragozaserrano.compose.utils.TextType
+import com.mzaragozaserrano.presentation.R
+
+@Composable
+fun SmallText(
+    modifier: Modifier = Modifier,
+    color: Color = MaterialTheme.colorScheme.onPrimary,
+    maxLines: Int = 1,
+    text: String,
+    textAlign: TextAlign = TextAlign.Start,
+    textType: TextType = TextType.Adjust(FontSize.Small.size),
+) {
+    BaseText(
+        modifier = modifier,
+        color = color,
+        fontSize = FontSize.Small.size,
+        maxLines = maxLines,
+        text = text,
+        textAlign = textAlign,
+        textType = textType
+    )
+}
+
+@Composable
+fun SmallText(
+    modifier: Modifier = Modifier,
+    color: Color = MaterialTheme.colorScheme.onPrimary,
+    maxLines: Int = 1,
+    @StringRes textId: Int,
+    textAlign: TextAlign = TextAlign.Start,
+    textType: TextType = TextType.Adjust(FontSize.Small.size),
+) {
+    BaseText(
+        modifier = modifier,
+        color = color,
+        fontSize = FontSize.Small.size,
+        maxLines = maxLines,
+        text = stringResource(id = textId),
+        textAlign = textAlign,
+        textType = textType
+    )
+}
+
+@Composable
+fun SmallText(
+    modifier: Modifier = Modifier,
+    color: Color = MaterialTheme.colorScheme.onPrimary,
+    maxLines: Int = 1,
+    text: AnnotatedString,
+    textAlign: TextAlign = TextAlign.Start,
+    textType: TextType = TextType.Adjust(FontSize.Small.size),
+) {
+    BaseText(
+        modifier = modifier,
+        color = color,
+        fontSize = FontSize.Small.size,
+        maxLines = maxLines,
+        text = text,
+        textAlign = textAlign,
+        textType = textType
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun SmallTextPrev() {
+    SmallText(text = stringResource(id = R.string.hello_world))
+}
