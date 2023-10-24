5d59e8b4133bdeb16b5ac16f002095c92011c1a5
Miguel Zaragoza Serrano <mzaragozaserrano@gmail.com>
2023-10-24 13:02:48 +0200

    adding flow extension

---
+++ domain/src/main/java/com/mzaragozaserrano/domain/utils/Flow.kt
@@ -0,0 +1,9 @@
+package com.mzaragozaserrano.domain.utils
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+fun <T> T.toFlowResult(): Flow<T> {
+    val value = this
+    return flow { emit(value) }
+}
