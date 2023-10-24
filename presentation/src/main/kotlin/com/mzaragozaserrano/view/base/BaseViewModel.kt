28e1957ec447159e098c86b2d32bebcc391b0d17
Miguel Zaragoza Serrano <mzaragozaserrano@gmail.com>
2023-10-24 11:45:08 +0200

    adding base viewmodel

---
+++ presentation/src/main/kotlin/com/mzaragozaserrano/view/base/BaseViewModel.kt
@@ -0,0 +1,25 @@
+package com.mzaragozaserrano.view.base
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+abstract class BaseViewModel<S, I> : ViewModel() {
+
+    private val initialState: S by lazy { createInitialState() }
+    abstract fun createInitialState(): S
+
+    private val _mutableViewState = MutableStateFlow(initialState)
+    val mutableViewState: StateFlow<S> get() = _mutableViewState.asStateFlow()
+
+    val intentFlow = MutableSharedFlow<I>()
+
+    open fun handleIntent() {}
+
+    fun collectState(state: S) {
+        _mutableViewState.value = state
+    }
+
+}
