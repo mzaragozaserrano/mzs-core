cb53251e5d5bc82152fced6e792c824025d66b34
Miguel Zaragoza Serrano <mzaragozaserrano@gmail.com>
2023-10-24 09:21:37 +0200

    creating minimal button view

---
+++ presentation/src/main/kotlin/com/mzaragozaserrano/view/utils/BaseComposeView.kt
@@ -0,0 +1,26 @@
+package com.mzaragozaserrano.view.utils
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.AbstractComposeView
+
+abstract class BaseComposeView<T : Any> @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0,
+) : AbstractComposeView(context, attrs, defStyleAttr) {
+
+    protected lateinit var item: T
+
+    abstract var content: @Composable () -> Unit
+
+    fun initComponent(item: T) {
+        this.item = item
+    }
+
+    @Composable
+    override fun Content() {
+        if (this::item.isInitialized) {
+            content()
+        }
+    }
+}
---
+++ presentation/src/main/kotlin/com/mzaragozaserrano/view/vo/MinimalButtonVO.kt
@@ -0,0 +1,13 @@
+package com.mzaragozaserrano.view.vo
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.ui.graphics.Color
+
+data class MinimalButtonVO(
+    @DrawableRes val iconId: Int,
+    val iconTint: Color = Color.Black,
+    val textColor: Color = Color.Black,
+    @StringRes val textId: Int,
+    val onButtonClicked: () -> Unit,
+)
