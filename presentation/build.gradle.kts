43bb6414465c70da96f7a8f495078bc6456b9e52
Miguel Zaragoza Serrano <mzaragozaserrano@gmail.com>
2023-10-26 19:57:44 +0200

    adding toolbar and drawer layout

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -43,6 +43,8 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.4")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.4")
 
     //----- COMPOSE ----------/
     implementation("androidx.compose.ui:ui")
