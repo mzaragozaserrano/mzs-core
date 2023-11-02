955a6e6c1111c570f9eddabcca3f9a9a65bc4c25
Miguel Zaragoza Serrano <mzaragozaserrano@gmail.com>
2023-11-02 20:40:46 +0100

    improving base activity

--- presentation/src/main/kotlin/com/mzaragozaserrano/presentation/view/base/BaseActivity.kt
+++ presentation/src/main/kotlin/com/mzaragozaserrano/presentation/view/base/BaseActivity.kt
@@ -5,11 +5,20 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
+import androidx.annotation.ColorRes
+import androidx.annotation.StringRes
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.viewbinding.ViewBinding
+import com.mzaragozaserrano.presentation.R
+import com.mzaragozaserrano.presentation.view.utils.emptyText
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
@@ -23,7 +32,7 @@ abstract class BaseActivity<S, I, VB : ViewBinding, VM : BaseViewModel<S, I>> :
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        initSetup()
+        binding.initSetup()
         lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
                 viewModel.mutableViewState.collectLatest { state ->
@@ -34,13 +43,70 @@ abstract class BaseActivity<S, I, VB : ViewBinding, VM : BaseViewModel<S, I>> :
         setContentView(binding.root)
     }
 
-    private fun initSetup() {
+    private fun VB.initSetup() {
         setUpListeners()
         setUpView()
     }
 
-    open fun setUpListeners() {}
-    open fun setUpView() {}
+    open fun VB.setUpListeners() {}
+    open fun VB.setUpView() {}
+
+    fun setUpMenuToolbar(
+        activity: Activity,
+        allowGoBack: Boolean,
+        drawerLayout: DrawerLayout,
+        @ColorRes scrimColorId: Int,
+        @ColorRes titleColorId: Int,
+        @StringRes titleTextId: Int?,
+        toolbar: Toolbar,
+        @ColorRes toolbarBackgroundColorId: Int,
+        toolbarTitle: AppCompatTextView?,
+    ) {
+
+        val iconNavigationId = if(allowGoBack) R.drawable.core_ic_arrow_back else R.drawable.core_ic_menu
+
+        val scrimColor = ContextCompat.getColor(applicationContext, scrimColorId)
+        val titleColor = ContextCompat.getColor(applicationContext, titleColorId)
+        val toolbarBackgroundColor =
+            ContextCompat.getColor(applicationContext, toolbarBackgroundColorId)
+
+        val toggle = ActionBarDrawerToggle(
+            activity,
+            drawerLayout,
+            toolbar,
+            R.string.core_open_navigation_drawer,
+            R.string.core_close_navigation_drawer
+        )
+
+        titleTextId?.let { titleId ->
+            toolbarTitle?.apply {
+                text = getString(titleId)
+                setTextColor(titleColor)
+                setBackgroundColor(toolbarBackgroundColor)
+            }
+        }
+
+        with(toolbar) {
+            title = emptyText
+            setBackgroundColor(toolbarBackgroundColor)
+            setNavigationIcon(iconNavigationId)
+            when (iconNavigationId) {
+                R.drawable.core_ic_menu -> {
+                    drawerLayout.apply {
+                        setScrimColor(scrimColor)
+                        drawerElevation = 50f
+                    }
+                    toggle.syncState()
+                }
+
+                R.drawable.core_ic_arrow_back -> {
+                    setNavigationOnClickListener {
+                        onBackPressedDispatcher.onBackPressed()
+                    }
+                }
+            }
+        }
+    }
 
     fun emitAction(intent: I) {
         lifecycleScope.launch {
