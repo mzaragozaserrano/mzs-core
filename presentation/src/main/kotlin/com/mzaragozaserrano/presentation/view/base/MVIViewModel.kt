62baafc80f5c5e4d317c7142e4a929b7fea73510
Miguel Zaragoza Serrano <mzaragozaserrano@gmail.com>
2023-11-13 18:43:33 +0100

    refactoring name

--- presentation/src/main/kotlin/com/mzaragozaserrano/presentation/view/base/MVIViewModel.kt
+++ presentation/src/main/kotlin/com/mzaragozaserrano/presentation/view/base/MVIViewModel.kt
@@ -6,7 +6,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
-abstract class BaseViewModel<S, I> : ViewModel() {
+abstract class MVIViewModel<S, I> : ViewModel() {
 
     private val initialState: S by lazy { createInitialState() }
     abstract fun createInitialState(): S
