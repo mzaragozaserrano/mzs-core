f47beb0b3397e55943d9196652c6da9c49e83b65
Miguel Zaragoza Serrano <mzaragozaserrano@gmail.com>
2023-11-21 23:16:35 +0100

    modifying size loading urlimage

--- presentation/src/main/kotlin/com/mzaragozaserrano/presentation/compose/components/images/UrlImage.kt
+++ presentation/src/main/kotlin/com/mzaragozaserrano/presentation/compose/components/images/UrlImage.kt
@@ -1,11 +1,14 @@
 package com.mzaragozaserrano.presentation.compose.components.images
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import coil.compose.SubcomposeAsyncImage
 import coil.request.ImageRequest
 import coil.transform.RoundedCornersTransformation
@@ -25,7 +28,10 @@ fun UrlImage(
         contentDescription = contentDescription,
         contentScale = contentScale,
         loading = {
-            LottieImage(animation = R.raw.core_image_loading)
+            LottieImage(
+                modifier = Modifier.padding(all = 120.dp),
+                animation = R.raw.core_image_loading
+            )
         },
         model = ImageRequest.Builder(context = LocalContext.current)
             .data(url)
@@ -34,5 +40,13 @@ fun UrlImage(
                 ?: RoundedCornersTransformation(radius = 0f))
             .build()
     )
+
+}
+
+@Preview
+@Composable
+private fun UrlImagePrev() {
+
+    UrlImage(url = "https://picsum.photos/200")
 
 }
