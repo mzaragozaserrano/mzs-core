41d3d6806d0d66438962e6b518d935a55b57c5bd
mzaragoza <miguel.zaragozaserrano@plexus.es>
2024-12-24 14:55:52 +0100

    adding loading animation for waiting to click again

--- src/main/kotlin/com/mzs/core/presentation/components/compose/buttons/PushedButton.kt
+++ src/main/kotlin/com/mzs/core/presentation/components/compose/buttons/PushedButton.kt
@@ -1,21 +1,28 @@
 package com.mzs.core.presentation.components.compose.buttons
 
-import android.util.Log
 import android.view.MotionEvent
+import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInteropFilter
@@ -25,68 +32,120 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
+import com.mzs.core.presentation.utils.extensions.conditional
 
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun PushedButton(
     modifier: Modifier = Modifier,
     buttonBackgroundColor: Color,
+    durationMillisBlockingButton: Int? = null,
     text: String,
     textColor: Color,
     textStyle: TextStyle,
-    onButtonClicked: () -> Unit,
+    onButtonClicked: () -> Unit
 ) {
 
+    //Pressed animation
+    var buttonSize by remember { mutableStateOf(value = IntSize.Zero) }
     var isPressed by remember { mutableStateOf(value = false) }
-    var elementSize by remember { mutableStateOf(IntSize.Zero) }
+    var isLaunchingAction by remember { mutableStateOf(value = false) }
     var isClickAvailable by remember { mutableStateOf(value = true) }
-    val scale = animateFloatAsState(targetValue = if (isPressed) 0.93f else 1f, label = "")
+    val scale = animateFloatAsState(
+        targetValue = if (isPressed) 0.93f else 1f,
+        label = ""
+    )
 
-    Button(
+    //Loading animation
+    val progress = remember { Animatable(initialValue = 0f) }
+    var textSize by remember { mutableStateOf(value = IntSize.Zero) }
+
+    LaunchedEffect(key1 = isLaunchingAction, key2 = progress) {
+        if (durationMillisBlockingButton != null) {
+            if (isLaunchingAction) {
+                progress.animateTo(
+                    targetValue = 1f,
+                    animationSpec = tween(durationMillis = durationMillisBlockingButton)
+                )
+            }
+            if (progress.value == 1f) {
+                progress.snapTo(targetValue = 0f)
+                isLaunchingAction = false
+            }
+        }
+    }
+
+    Box(
         modifier = modifier
             .scale(scale = scale.value)
-            .pointerInteropFilter { motionEvent ->
-                when (motionEvent.action) {
-                    MotionEvent.ACTION_DOWN -> {
-                        isPressed = true
-                    }
+            .conditional(condition = progress.isRunning.not() || durationMillisBlockingButton == null) {
+                pointerInteropFilter { motionEvent ->
+                    when (motionEvent.action) {
+                        MotionEvent.ACTION_DOWN -> {
+                            isPressed = true
+                        }
 
-                    MotionEvent.ACTION_MOVE -> {
-                        if(isPressed) {
-                            val x = motionEvent.x.toInt()
-                            val y = motionEvent.y.toInt()
-                            isPressed =
-                                x in 0 until elementSize.width && y in 0 until elementSize.height
-                            isClickAvailable = false
+                        MotionEvent.ACTION_MOVE -> {
+                            if (isPressed) {
+                                val x = motionEvent.x.toInt()
+                                val y = motionEvent.y.toInt()
+                                isPressed =
+                                    x in 0 until buttonSize.width && y in 0 until buttonSize.height
+                                isClickAvailable = isPressed
+                            }
+                        }
+
+                        MotionEvent.ACTION_UP -> {
+                            if (isClickAvailable) {
+                                isPressed = false
+                                isLaunchingAction = true
+                                onButtonClicked()
+                            } else {
+                                isClickAvailable = true
+                            }
                         }
                     }
-
-                    MotionEvent.ACTION_UP -> {
-                        if (isClickAvailable) {
-                            isPressed = false
-                            onButtonClicked()
-                        } else {
-                            isClickAvailable = true
-                        }
-                    }
+                    true
                 }
-                true
             }
             .onGloballyPositioned { layoutCoordinates ->
-                elementSize = layoutCoordinates.size
-            },
-        colors = ButtonDefaults.buttonColors(containerColor = buttonBackgroundColor),
-        shape = RoundedCornerShape(size = 8.dp),
-        onClick = {}
+                buttonSize = layoutCoordinates.size
+            }
+            .clip(shape = RoundedCornerShape(size = 8.dp))
     ) {
-        Text(
+        Box(
             modifier = Modifier
-                .fillMaxWidth(),
-            color = textColor,
-            style = textStyle,
-            text = text.uppercase(),
-            textAlign = TextAlign.Center
+                .fillMaxWidth()
+                .height(height = textSize.height.dp)
+                .background(color = if (progress.value == 0f) buttonBackgroundColor else Color.LightGray)
         )
+        Box(
+            modifier = Modifier
+                .fillMaxWidth(fraction = progress.value)
+                .height(height = textSize.height.dp)
+                .background(color = buttonBackgroundColor)
+        )
+        if (progress.isRunning) {
+            CircularProgressIndicator(
+                modifier = Modifier
+                    .size(size = 32.dp)
+                    .align(alignment = Alignment.Center),
+                color = textColor.copy(alpha = 0.2f)
+            )
+        } else {
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(alignment = Alignment.Center)
+                    .onGloballyPositioned { layoutCoordinates ->
+                        textSize = layoutCoordinates.size
+                    },
+                color = textColor,
+                style = textStyle,
+                text = text.uppercase(),
+                textAlign = TextAlign.Center
+            )
+        }
     }
 
 }
@@ -95,10 +154,11 @@ fun PushedButton(
 @Composable
 private fun PushedButtonPrev() {
     PushedButton(
-        buttonBackgroundColor = Color.LightGray,
+        buttonBackgroundColor = Color.Red,
+        durationMillisBlockingButton = 3000,
         textColor = Color.Black,
         text = "Accept",
-        textStyle = MaterialTheme.typography.titleSmall
+        textStyle = MaterialTheme.typography.titleMedium
     ) {
         //Here will go the action when clicking on the button
     }
