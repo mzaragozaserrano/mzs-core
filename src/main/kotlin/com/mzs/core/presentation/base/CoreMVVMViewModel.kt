7ce9fc086e67284f5d7e433d8082afc4411c8727
Miguel Zaragoza Serrano <mzaragozaserrano@gmail.com>
2025-01-07 17:17:33 +0100

    adding channel to manage navigation in viewmodels

--- src/main/kotlin/com/mzs/core/presentation/base/CoreMVVMViewModel.kt
+++ src/main/kotlin/com/mzs/core/presentation/base/CoreMVVMViewModel.kt
@@ -1,16 +1,30 @@
 package com.mzs.core.presentation.base
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 abstract class CoreMVVMViewModel<State> : ViewModel() {
 
     private val _uiState by lazy { MutableStateFlow(value = createInitialState()) }
     val uiState = _uiState.asStateFlow()
 
+    private val _navigationCompose = Channel<Any?>(capacity = Channel.BUFFERED)
+    val navigationCompose: Flow<Any?> = _navigationCompose.receiveAsFlow()
+
     protected abstract fun createInitialState(): State
+
+    protected fun onEmitNavigation(element: Any?) {
+        viewModelScope.launch {
+            _navigationCompose.send(element = element)
+        }
+    }
 
     protected fun onUpdateUiState(update: State.() -> State) {
         _uiState.update { it.update() }
---
+++ src/main/kotlin/com/mzs/core/presentation/utils/functions/LaunchedEffect.kt
@@ -0,0 +1,22 @@
+package com.mzs.core.presentation.utils.functions
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.flow.Flow
+
+@Composable
+fun <T> SingleEventEffect(
+    sideEffectFlow: Flow<T>,
+    lifeCycleState: Lifecycle.State = Lifecycle.State.STARTED,
+    collector: (T) -> Unit,
+) {
+    val lifecycleOwner = LocalLifecycleOwner.current
+    LaunchedEffect(sideEffectFlow) {
+        lifecycleOwner.repeatOnLifecycle(lifeCycleState) {
+            sideEffectFlow.collect(collector)
+        }
+    }
+}
