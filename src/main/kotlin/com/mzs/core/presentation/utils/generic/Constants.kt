76b57909d8df9761ed4d3a93c636b73ecec59288
Miguel Zaragoza Serrano <mzaragozaserrano@gmail.com>
2025-01-07 15:56:12 +0100

    adding constant

--- src/main/kotlin/com/mzs/core/presentation/utils/generic/Constants.kt
+++ src/main/kotlin/com/mzs/core/presentation/utils/generic/Constants.kt
@@ -2,5 +2,6 @@ package com.mzs.core.presentation.utils.generic
 
 import android.util.Log
 
-const val emptyText: String = ""
 val emptyLambda: () -> Unit = { Log.d("EMPTY_LAMBDA", emptyText) }
+const val emptyText: String = ""
+const val nullInt: Int = -1
