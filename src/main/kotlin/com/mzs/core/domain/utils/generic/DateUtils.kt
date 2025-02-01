b877997535683bdb1bc9b00bd1525b1ccf62a826
Miguel Zaragoza Serrano <mzaragozaserrano@gmail.com>
2025-02-01 21:41:05 +0100

    updating dateutils

--- src/main/kotlin/com/mzs/core/domain/utils/generic/DateUtils.kt
+++ src/main/kotlin/com/mzs/core/domain/utils/generic/DateUtils.kt
@@ -1,6 +1,10 @@
 package com.mzs.core.domain.utils.generic
 
+import com.mzs.core.R
+import com.mzs.core.data.datasources.local.ResourcesDataSource
 import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import java.util.Date
 import java.util.Locale
 
@@ -9,9 +13,44 @@ const val ddMMyyyy = "dd/MM/yyyy"
 const val ddMMyyyy_HHmm = "dd/MM/yyyy HH:mm"
 const val HHmm = "HH:mm"
 
-class DateUtils {
-    fun getCurrentDate(formatOut: String): String {
-        val sdf = SimpleDateFormat(formatOut, Locale.getDefault())
-        return sdf.format(Date())
+class DateUtils(private val resourcesDataSource: ResourcesDataSource) {
+
+    fun getCurrentDate(formatOut: String): String? {
+        return try {
+            val sdf = SimpleDateFormat(formatOut, Locale.getDefault())
+            sdf.format(Date())
+        } catch (e: Exception) {
+            null
+        }
     }
+
+    fun convertStringToLocalDate(dateString: String, format: String): LocalDate? {
+        return try {
+            val formatter = DateTimeFormatter.ofPattern(format)
+            LocalDate.parse(dateString, formatter)
+        } catch (e: Exception) {
+            null
+        }
+    }
+
+    fun formatDateToFriendlyString(date: LocalDate?): String {
+        return if (date == null) {
+            resourcesDataSource.getStringFromResource(resId = R.string.date_today)
+        } else {
+            when {
+                date.isEqual(LocalDate.now()) -> {
+                    resourcesDataSource.getStringFromResource(resId = R.string.date_today)
+                }
+
+                date.isEqual(LocalDate.now().minusDays(1)) -> {
+                    resourcesDataSource.getStringFromResource(resId = R.string.date_yesterday)
+                }
+
+                else -> {
+                    date.format(DateTimeFormatter.ofPattern(ddMMyyyy))
+                }
+            }
+        }
+    }
+
 }
